55d239229829d37f0a015d734e82c327ebd89a90
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-01-31 13:38:45 +0700

    Add files via upload

---
+++ HTMLDocument/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace HTMLDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(6, 14);
+            this.urlTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(299, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(1, 85);
+            this.resultListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(982, 498);
+            this.resultListBox.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(333, 598);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(293, 16);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 626);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YanexParse - Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ HTMLDocument/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTMLDocument
+{
+    public partial class Form1 : Form
+    {   
+        private bool checkVariable = false;
+        private bool checkOne = false;
+        private bool checkTwo = false;
+        public Form1()
+        {
+            InitializeComponent();
+            urlTextBox.Text = @"https://dzen.ru/news?issue_tld=ru";
+            checkBox1.Text = "Получить список всех категорий";
+            checkBox2.Text = "Получить список наименований последних новостей";
+        }
+        private void getCaptionCategories(string url)
+        {   
+            resultListBox.Items.Clear();
+            progressBar1.Value = 0;
+            string titleContent = "";
+
+            // загружаем страничку в браузер
+            WebBrowser browser = new WebBrowser();
+            browser.Navigate(urlTextBox.Text);
+            while (browser.ReadyState != WebBrowserReadyState.Complete)
+            {
+                while (progressBar1.Value != progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                }
+                Application.DoEvents();
+            }
+                   
+            //Верхнее меню, список категорий
+            HtmlElementCollection elementsByTagName =
+                    browser.Document.GetElementsByTagName("span");
+
+            foreach (HtmlElement element in elementsByTagName)
+            {
+                titleContent += element.InnerHtml;
+            }
+
+            int startCut = titleContent.IndexOf("Ещё");
+            int endCut = titleContent.LastIndexOf("Ещё");
+
+            string removeFromTilteContentOne = titleContent.Substring(startCut, endCut);
+            int cutTags = removeFromTilteContentOne.IndexOf("<");
+
+            string newString = removeFromTilteContentOne.Remove(cutTags);
+            newString = newString.Replace("Ещё", "_");
+            newString = newString.Remove(0, 1);
+
+            string[] array = newString.Split('_');
+            for (int i = 0; i < array.Length; i++)
+            {
+                resultListBox.Items.Add(array[i]);
+            }
+
+            browser.Dispose();
+        }
+        private void getTitleAllNews(string url)
+        {
+            progressBar1.Value = 0;
+            resultListBox.Items.Clear();
+            string titleContent = "";
+
+            // загружаем страничку в браузер
+            WebBrowser browser = new WebBrowser();
+            browser.Navigate(urlTextBox.Text);
+            while (browser.ReadyState != WebBrowserReadyState.Complete)
+            {
+                while (progressBar1.Value != progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                }
+                Application.DoEvents();
+            }
+
+            // получаем все теги <h2> и перебираем их
+            HtmlElementCollection elementsByTagName =
+                 browser.Document.GetElementsByTagName("h2");
+            foreach (HtmlElement element in elementsByTagName)
+            {
+                resultListBox.Items.Add(element.OuterText);
+                //titleContent += element.OuterText + "\n\n";
+            }
+
+            browser.Dispose();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            if (checkOne || checkTwo)
+            {
+                checkVariable = true;
+
+                if (checkOne)
+                {
+                    getCaptionCategories(urlTextBox.Text);
+                    while (progressBar1.Value != progressBar1.Maximum)
+                        progressBar1.Value++;
+                }
+                if (checkTwo)
+                {
+                    getTitleAllNews(urlTextBox.Text);
+                    while (progressBar1.Value != progressBar1.Maximum)
+                        progressBar1.Value++;
+                }
+            }
+            else
+            {
+                if (!checkVariable)
+                {
+                    MessageBox.Show("Необходимо сделать выбор");
+                    return;
+                }
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {   
+            if (checkBox1.Checked)
+            {
+                checkBox2.Visible = false;
+                checkOne = true;
+                checkTwo = false;
+            }
+            else
+            {
+                checkBox2.Visible = true;
+                checkTwo = true;
+                checkOne = false;
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Visible = false;
+                checkOne = false;
+                checkTwo = true;
+            }
+            else
+            {
+                checkBox1.Visible = true;
+                checkOne = true;
+                checkTwo = false;
+            }
+        }
+    }
+}
